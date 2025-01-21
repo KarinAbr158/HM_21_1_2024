996c6fd21b84a5d0213de263a6f4192f4c0e5bd7
Karin <abramowitzkarin@gmail.com>
2025-01-21 14:44:00 +0200

    boe jiden

---
+++ app/src/main/java/com/example/hm_21_1_2024/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hm_21_1_2024;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] mData;
+    int dLength = 50;
+    RecyclerView bitch;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mData = new int[dLength];
+        for(int i = 0; i < dLength; i++){
+            mData[i] = i;
+        }
+        bitch = findViewById(R.id.nigga);
+    }
+}
--- app/src/main/java/com/example/hm_21_1_2024/MyG.java
+++ app/src/main/java/com/example/hm_21_1_2024/MyG.java
@@ -1,2 +1,48 @@
-package com.example.hm_21_1_2024;public class MyG {
+package com.example.hm_21_1_2024;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyG extends RecyclerView.Adapter<MyG.MyViewHolder> {
+
+    int[] mData;
+    Context mContext;
+    public MyG(Context context, int[] data){
+        this.mContext = context;
+        this.mData = data;
+    }
+    @NonNull
+    @Override
+    public MyG.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyG.MyViewHolder holder, int position) {
+        holder.tv.setText(String.valueOf(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mData.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+        ImageView img;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.tv = itemView.findViewById(R.id.itemTextView);
+            this.img = itemView.findViewById(R.id.imageView);
+        }
+    }
 }
